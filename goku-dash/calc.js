81ebfafc301270bf530152c2f598496db77331b3
Akio Takahashi <akiotakahashi@outlook.com>
2024-11-17 14:07:52 +0900

    refactor: common functions

--- goku-dash/calc.js
+++ goku-dash/calc.js
@@ -2,66 +2,6 @@ var questions = [];
 var n_asked = 0;
 var n_solved = -1;
 
-var n_timer = null;
-var start_time = null;
-
-function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function randomChoice(candidates) {
-    return candidates[randomInt(0, candidates.length)];
-}
-
-function fisherYatesShuffle(arr) {
-    for(let i = arr.length - 1 ; i > 0; i--){
-        let j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-}
-
-function setStatus(text) {
-    let status = document.getElementById('status');
-    status.innerText = text;
-}
-
-function onTimer() {
-    let now = new Date();
-    let dt = (now.getTime() - start_time.getTime()) / 1000;
-    let min = document.getElementById('minutes');
-    let sec = document.getElementById('seconds');
-    min.innerText = Math.floor(dt / 60);
-    sec.innerText = Math.floor(dt % 60);
-}
-
-function hideStopwatch() {
-    let sw = document.getElementById('stopwatch');
-    sw.hidden = true;
-}
-
-function stopStopwatch() {
-    if (n_timer === null) {
-        return;
-    }
-    clearInterval(n_timer);
-    n_timer = null;
-    let sw = document.getElementById('stopwatch');
-    sw.classList.remove("running");
-    sw.classList.add("fixed");
-    sw.hidden = false;
-}
-
-function startStopwatch() {
-    stopStopwatch();
-    start_time = new Date();
-    n_timer = window.setInterval(onTimer, 1000);
-    let sw = document.getElementById('stopwatch');
-    sw.classList.remove("fixed");
-    sw.classList.add("running");
-    sw.hidden = false;
-    onTimer();
-}
-
 function updateQuestion(prefix, ix, predicate) {
     let div = document.getElementById(prefix);
     if (ix < 0 || ix >= questions.length) {
